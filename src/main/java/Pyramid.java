55e120afc6cc79126153d32fbaf777a6795b29ae
sajjad <sajjad.goodarzi1990@gmail.com>
2023-03-08 04:52:43 +0000

    done

--- src/main/java/Pyramid.java
+++ src/main/java/Pyramid.java
@@ -28,6 +28,13 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
-        return "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 }
